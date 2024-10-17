ec5c18bc3938c9b156f3bfa2bf21331645cf2d3c
Utkarsha Turambekar <utkarshat200318@gmail.com>
2024-10-17 12:37:03 +0530

    Express setup

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express=require('express');
+const dotenv=require('dotenv').config();
+
+
 const app=express();
 
-const port=5000;
+const port=process.env.PORT || 6000;
 
 app.listen(port,()=>{
     console.log(`Port is listening at Port numbers ${port}`);
